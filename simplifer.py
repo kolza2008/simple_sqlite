73d33a52e4738af3cfc8cf53c414f1b559f033b1
kolza2008 <33565304+kolza2008@users.noreply.github.com>
2021-02-19 17:57:13 +0300

    Update simplifer.py

--- simplifer.py
+++ simplifer.py
@@ -2,12 +2,12 @@ class AdvancedDispatcher:
     def __init__(self, con, cur):
         self.conn = con
         self.cur = cur
-
+        
         self.inits = [] #list with init methods
 
     def write(self, func):
         "Decorator for create methods for edit or write to sqlite database"
-        print('Database method decorated')
+        #print('Database method decorated')
         def db_function():
             data = func()
             self.cur.execute(data)
@@ -17,21 +17,20 @@ class AdvancedDispatcher:
 
     def read(self, func):
         "Decorator for create methods for read data from sqlite database"
-        print('Database method decorated')
+        #print('Database method decorated')
         def db_function():
             data = func()
             cur.execute(data)
-            return cur.fetchall()
         return db_function
 
     def init(self, func):
         "Decorator for register init methods"
-        print('New init method')
+        #print('New init method')
         self.inits.append(func)
         def db_function():
             func()
         return db_function
-
+    
     def start(self):
         "Method that starts all init methods"
         try:
